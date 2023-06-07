62b4cb3bcff5367a356c13603f0ee51e1dac2c96
Ojeyinka Ifeoluwa Samuel <131479901+samojeyinka@users.noreply.github.com>
2023-06-07 08:35:39 -1200

    Update CoinItem.jsx

--- src/components/CoinItem.jsx
+++ src/components/CoinItem.jsx
@@ -38,7 +38,7 @@ const CoinItem = ({ coin }) => {
       {/* Save coin button */}
       <td onClick={saveCoin}>
         <i className='star-icon'>
-          {savedCoin ? <BiStar size={18} /> : <BiStar size={20}/>}
+          {savedCoin ? <BiStar size={20}  style={{color:'#ffa500'}}/> : <BiStar size={18} />}
         </i>
       </td>
       {/* Market cap rank */}
